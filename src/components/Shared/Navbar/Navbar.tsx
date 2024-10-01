327869f3c1616769914ccdecf534ab750cb56a30
inzamulhaque <mdihalif@gmail.com>
2024-10-02 00:00:18 +0600

    use stack component

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,7 +1,22 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const Navbar = () => {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Stack py={2} direction={"row"} justifyContent={"space-between"}>
+          <Typography variant="h5" component="h1" fontWeight={600}>
+            P
+            <Box component={"span"} color={"primary.main"}>
+              H
+            </Box>{" "}
+            Health Care
+          </Typography>
+        </Stack>
+      </Container>
+    </>
+  );
 };
 
 export default Navbar;
