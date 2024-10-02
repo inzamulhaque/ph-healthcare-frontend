94079420a6cad3ec68cf5d1bc0a4ca0c3f26f2e3
inzamulhaque <mdihalif@gmail.com>
2024-10-02 12:33:58 +0600

    create navbar

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,18 +1,46 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <>
       <Container>
-        <Stack py={2} direction={"row"} justifyContent={"space-between"}>
-          <Typography variant="h5" component="h1" fontWeight={600}>
+        <Stack
+          py={2}
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Typography variant="h4" component={Link} href="/" fontWeight={600}>
             P
             <Box component={"span"} color={"primary.main"}>
               H
             </Box>{" "}
             Health Care
           </Typography>
+
+          <Stack direction={"row"} gap={4} justifyContent={"space-between"}>
+            <Typography component={Link} href="/consultation">
+              Consultation
+            </Typography>
+            <Typography component={Link} href="/login">
+              Health Plans
+            </Typography>
+            <Typography component={Link} href="/login">
+              Medicine
+            </Typography>
+            <Typography component={Link} href="/login">
+              Diagnostics
+            </Typography>
+            <Typography component={Link} href="/login">
+              NGOs
+            </Typography>
+          </Stack>
+
+          <Button component={Link} href="/login">
+            Login
+          </Button>
         </Stack>
       </Container>
     </>
