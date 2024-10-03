8e5175c94d65484900584f8dd2292ee6af92164e
inzamulhaque <mdihalif@gmail.com>
2024-10-03 17:49:48 +0600

    hero section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,11 @@
+import Herosction from "@/components/UI/HomePage/HeroSection/Herosction";
 import { Button } from "@mui/material";
 import React from "react";
 
 const HomePage = () => {
   return (
     <>
-      <h1>Home Page</h1>
-      <Button>Button</Button>
+      <Herosction />
     </>
   );
 };
