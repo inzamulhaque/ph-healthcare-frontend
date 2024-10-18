417940bda1864c1d2c8acec5595a27fead3a5b85
inzamulhaque <mdihalif@gmail.com>
2024-10-18 16:52:20 +0600

    setup redux

--- src/lib/Providers/Providers.tsx
+++ src/lib/Providers/Providers.tsx
@@ -2,11 +2,15 @@
 
 import { ThemeProvider } from "@mui/material";
 import theme from "../theme/theme";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </Provider>
     </>
   );
 };
