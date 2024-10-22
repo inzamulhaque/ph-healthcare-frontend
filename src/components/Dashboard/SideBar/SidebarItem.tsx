4989592341a3066c88b22de98df0757c451d297f
inzamulhaque <mdihalif@gmail.com>
2024-10-22 19:47:26 +0600

    Applying Active Link Style

--- src/components/Dashboard/SideBar/SidebarItem.tsx
+++ src/components/Dashboard/SideBar/SidebarItem.tsx
@@ -6,13 +6,30 @@ import {
   ListItemText,
 } from "@mui/material";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const SidebarItem = ({ item }: { item: IDrawerItem }) => {
+  const linkPath = `/dashboard/${item.path}`;
+  const pathname = usePathname();
+
   return (
     <>
-      <Link href={item.path}>
-        <ListItem disablePadding>
+      <Link href={linkPath}>
+        <ListItem
+          disablePadding
+          sx={{
+            ...(pathname === linkPath
+              ? {
+                  borderRight: "3px solid #1586FD",
+                  "& svg": {
+                    color: "#1586FD",
+                  },
+                }
+              : {}),
+            mb: 1,
+          }}
+        >
           <ListItemButton>
             <ListItemIcon>{item?.icon && <item.icon />}</ListItemIcon>
             <ListItemText primary={item.title} />
