639860b60b30ed9f4b1cf0e51d1bd5033c07ee58
inzamulhaque <mdihalif@gmail.com>
2024-10-24 12:20:38 +0600

    create modal

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Box, Button, Stack, TextField } from "@mui/material";
+import PHModal from "@/components/Shared/PHModal/PHModal";
 
 const SpecialtiesPage = () => {
   return (
     <>
-      <h1>specialties page</h1>
+      <Box>
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Button>Create Specialty</Button>
+          <PHModal />
+
+          <TextField size="small" placeholder="Search Speciaties" />
+        </Stack>
+      </Box>
     </>
   );
 };
