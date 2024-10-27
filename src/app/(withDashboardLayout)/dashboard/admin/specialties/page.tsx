186440abe4cb1d346896fb34fe5b2c49629ef9d0
inzamulhaque <mdihalif@gmail.com>
2024-10-27 23:43:29 +0600

    Displaying Specialty Data Using Data Grid Table

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -2,12 +2,26 @@
 
 import React, { useState } from "react";
 import { Box, Button, Stack, TextField } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import SpecialtyModal from "./components/SpeciatiesModal";
 import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
+import Image from "next/image";
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const { data, isLoading } = useGetAllSpecialtiesQuery({});
+
+  const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 170 },
+    {
+      field: "icon",
+      headerName: "Icon",
+      width: 170,
+      renderCell: ({ row }) => (
+        <Image src={row.icon} width={40} height={40} alt="icon" />
+      ),
+    },
+  ];
 
   return (
     <>
@@ -23,7 +37,13 @@ const SpecialtiesPage = () => {
           <TextField size="small" placeholder="Search Speciaties" />
         </Stack>
 
-        <Box></Box>
+        {!isLoading ? (
+          <Box>
+            <DataGrid rows={data} columns={columns} sx={{ border: 0 }} />
+          </Box>
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </Box>
     </>
   );
