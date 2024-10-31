a367aa4c9f3cb7ecb742d2848d65a963d30e0abb
inzamulhaque <mdihalif@gmail.com>
2024-10-31 18:18:45 +0600

    create schedule modal

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+"use client";
+
+import { Box, Button } from "@mui/material";
+import React, { useState } from "react";
+import ScheduleModal from "./components/ScheduleModal";
 
 const SchedulesPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [allSchedule, setAllSchedule] = useState<any>([]);
+
   return (
-    <div>
-      <h1>schedules page</h1>
-    </div>
+    <>
+      <Box>
+        <Button onClick={() => setIsModalOpen(true)}>Create Schedule</Button>
+        <ScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
+      </Box>
+    </>
   );
 };
 
