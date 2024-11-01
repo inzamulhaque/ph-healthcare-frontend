f46cb73c83cf903834ce5d0f601b8852ad844c1f
inzamulhaque <mdihalif@gmail.com>
2024-11-01 18:50:59 +0600

    show all schedules data

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
@@ -1,12 +1,57 @@
 "use client";
 
-import { Box, Button } from "@mui/material";
-import React, { useState } from "react";
+import { Box, Button, IconButton } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import ScheduleModal from "./components/ScheduleModal";
+import { useGetAllSchedulesQuery } from "@/redux/api/scheduleApi";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { TSchedule } from "@/types/schedule";
+import dateFormatter from "@/utils/dateFormatter";
+import dayjs from "dayjs";
 
 const SchedulesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [allSchedule, setAllSchedule] = useState<any>([]);
+  const { data, isLoading } = useGetAllSchedulesQuery({});
+
+  const schedules = data?.schedules;
+  const meta = data?.meta;
+
+  useEffect(() => {
+    const updateData = schedules?.map((schedule: TSchedule, index: number) => {
+      return {
+        sl: index + 1,
+        id: schedule?.id,
+        startDate: dateFormatter(schedule.startDate),
+        endDate: dateFormatter(schedule.endDate),
+        startTime: dayjs(schedule?.startDate).format("hh:mm a"),
+        endTime: dayjs(schedule?.endDate).format("hh:mm a"),
+      };
+    });
+    setAllSchedule(updateData);
+  }, [schedules]);
+
+  const columns: GridColDef[] = [
+    { field: "sl", headerName: "SL" },
+    { field: "startDate", headerName: "Date", flex: 1 },
+    { field: "startTime", headerName: "Start Time", flex: 1 },
+    { field: "endTime", headerName: "End Time", flex: 1 },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <IconButton aria-label="delete">
+            <DeleteIcon sx={{ color: "red" }} />
+          </IconButton>
+        );
+      },
+    },
+  ];
 
   return (
     <>
@@ -14,7 +59,13 @@ const SchedulesPage = () => {
         <Button onClick={() => setIsModalOpen(true)}>Create Schedule</Button>
         <ScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
 
-        <Box my={5}>Display Schedules</Box>
+        {!isLoading ? (
+          <Box my={2}>
+            <DataGrid rows={allSchedule ?? []} columns={columns} />
+          </Box>
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </Box>
     </>
   );
