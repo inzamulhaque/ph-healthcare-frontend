bdab5e0fb04eaf4ccf88e7256e8b50c7a56b3a06
inzamulhaque <mdihalif@gmail.com>
2024-11-04 22:45:20 +0600

    create update doctor info page

---
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/edit/[doctorId]/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+type TProps = {
+  params: Record<string, unknown>;
+};
+
+const DoctorUpdatePage = ({ params }: TProps) => {
+  return (
+    <>
+      <h1>Update Doctor Info</h1>
+    </>
+  );
+};
+
+export default DoctorUpdatePage;
