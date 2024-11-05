7107154f35bb7e951e9980009302e7c353d1345c
inzamulhaque <mdihalif@gmail.com>
2024-11-05 16:54:56 +0600

    show previous data

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/edit/[doctorId]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/edit/[doctorId]/page.tsx
@@ -14,9 +14,9 @@ type TProps = {
 };
 
 const DoctorUpdatePage = ({ params }: TProps) => {
-  const { data, isLoading } = useGetSingleDoctorQuery(
-    params?.doctorId as string
-  );
+  const id = params?.doctorId;
+
+  const { data, isLoading } = useGetSingleDoctorQuery(id as string);
 
   const handleUpdateDoctorInfo = async (values: FieldValues) => {
     try {
@@ -26,18 +26,17 @@ const DoctorUpdatePage = ({ params }: TProps) => {
   };
 
   const defaultValues = {
-    email: "",
-    name: "",
-    contactNumber: "",
-    address: "",
-    registrationNumber: "",
-    gender: "",
-    experience: 0,
-    apointmentFee: 0,
-    qualification: "",
-    currentWorkingPlace: "",
-    designation: "",
-    profilePhoto: "",
+    email: data?.email || "",
+    name: data?.name || "",
+    contactNumber: data?.contactNumber || "",
+    address: data?.address || "",
+    registrationNumber: data?.registrationNumber || "",
+    gender: data?.gender || "",
+    experience: data?.experience || 0,
+    apointmentFee: data?.apointmentFee || 0,
+    qualification: data?.qualification || "",
+    currentWorkingPlace: data?.currentWorkingPlace || "",
+    designation: data?.designation || "",
   };
 
   return (
@@ -46,105 +45,107 @@ const DoctorUpdatePage = ({ params }: TProps) => {
         <Typography component={"h5"} variant="h5" textAlign={"center"}>
           Update Doctor Info
         </Typography>
-        <PHForm submit={handleUpdateDoctorInfo} defaultValues={defaultValues}>
-          <Grid container spacing={2} sx={{ my: 5 }}>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="name"
-                label="Name"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="email"
-                type="email"
-                label="Email"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
+        {!isLoading && (
+          <PHForm submit={handleUpdateDoctorInfo} defaultValues={defaultValues}>
+            <Grid container spacing={2} sx={{ my: 5 }}>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="name"
+                  label="Name"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="email"
+                  type="email"
+                  label="Email"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="contactNumber"
+                  label="Contract Number"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="address"
+                  label="Address"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="registrationNumber"
+                  label="Registration Number"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="experience"
+                  type="number"
+                  label="Experience"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHSelectField
+                  items={Gender}
+                  name="gender"
+                  label="Gender"
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="apointmentFee"
+                  type="number"
+                  label="ApointmentFee"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="qualification"
+                  label="Qualification"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="currentWorkingPlace"
+                  label="Current Working Place"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <PHInput
+                  name="designation"
+                  label="Designation"
+                  fullWidth={true}
+                  sx={{ mb: 2 }}
+                />
+              </Grid>
             </Grid>
 
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="contactNumber"
-                label="Contract Number"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="address"
-                label="Address"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="registrationNumber"
-                label="Registration Number"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="experience"
-                type="number"
-                label="Experience"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHSelectField
-                items={Gender}
-                name="gender"
-                label="Gender"
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="apointmentFee"
-                type="number"
-                label="ApointmentFee"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="qualification"
-                label="Qualification"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="currentWorkingPlace"
-                label="Current Working Place"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <PHInput
-                name="designation"
-                label="Designation"
-                fullWidth={true}
-                sx={{ mb: 2 }}
-              />
-            </Grid>
-          </Grid>
-
-          <Button type="submit">Update</Button>
-        </PHForm>
+            <Button type="submit">Update</Button>
+          </PHForm>
+        )}
       </Box>
     </>
   );
