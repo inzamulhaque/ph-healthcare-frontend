6ea7e48e37bf0e8c17ee6793447cdf0ace796ec2
inzamulhaque <mdihalif@gmail.com>
2024-11-07 17:19:58 +0600

    select multiple slot

--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import PHModal from "@/components/Shared/PHModal/PHModal";
+import { useGetAllSchedulesQuery } from "@/redux/api/scheduleApi";
 import { Button } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
 import React, { useState } from "react";
+import MultipleSelectFieldChip from "./MultipleSelectFieldChip";
 
 type TProps = {
   open: boolean;
@@ -17,12 +19,35 @@ const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
     dayjs(new Date()).toISOString()
   );
 
+  const [selectedScheduleIds, setSelectedScheduleIds] = useState<string[]>([]);
+
+  const query: Record<string, unknown> = {};
+
+  if (!!selectedDate) {
+    query["startDate"] = dayjs(selectedDate)
+      .hour(0)
+      .minute(0)
+      .millisecond(0)
+      .toISOString();
+    query["endDate"] = dayjs(selectedDate)
+      .hour(23)
+      .minute(59)
+      .millisecond(999)
+      .toISOString();
+  }
+
+  const { data, isLoading } = useGetAllSchedulesQuery(query);
+  const schedules = data?.schedules;
+
+  console.log(schedules);
+
   return (
     <>
       <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
-            label="Controlled picker"
+            disablePast
+            label="Select Date"
             value={dayjs(selectedDate)}
             onChange={(newValue) =>
               setSelectedDate(dayjs(newValue).toISOString())
@@ -30,6 +55,12 @@ const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
             sx={{ width: "100%" }}
           />
         </LocalizationProvider>
+
+        <MultipleSelectFieldChip
+          schedules={schedules}
+          selectedScheduleIds={selectedScheduleIds}
+          setSelectedScheduleIds={setSelectedScheduleIds}
+        />
 
         <Button>Submit</Button>
       </PHModal>
