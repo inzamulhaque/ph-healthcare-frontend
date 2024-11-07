ef3518f61bc8f1dab7c1b2cd15a851e4c33be8db
inzamulhaque <mdihalif@gmail.com>
2024-11-07 12:44:47 +0600

    add date picker for doctor schedule

--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/components/DoctorScheduleModal.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import PHModal from "@/components/Shared/PHModal/PHModal";
 import { Button } from "@mui/material";
-import React from "react";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import React, { useState } from "react";
 
 type TProps = {
   open: boolean;
@@ -8,9 +13,24 @@ type TProps = {
 };
 
 const DoctorScheduleModal = ({ open, setOpen }: TProps) => {
+  const [selectedDate, setSelectedDate] = useState(
+    dayjs(new Date()).toISOString()
+  );
+
   return (
     <>
       <PHModal open={open} setOpen={setOpen} title="Create Doctor Schedule">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Controlled picker"
+            value={dayjs(selectedDate)}
+            onChange={(newValue) =>
+              setSelectedDate(dayjs(newValue).toISOString())
+            }
+            sx={{ width: "100%" }}
+          />
+        </LocalizationProvider>
+
         <Button>Submit</Button>
       </PHModal>
     </>
