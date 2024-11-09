3019bbd707435f2e837cb5066025e29256068d30
inzamulhaque <mdihalif@gmail.com>
2024-11-09 18:52:16 +0600

    show doctor profile

--- src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
+
+import {
+  useGetMYProfileQuery,
+  useUpdateMYProfileMutation,
+} from "@/redux/api/myProfile";
+import { Box, Button, Container, Grid } from "@mui/material";
+import Image from "next/image";
+import React, { useState } from "react";
+import DoctorInformation from "./components/DoctorInformations";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
 
 const DoctorProfulePage = () => {
-  return <></>;
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const { data, isLoading } = useGetMYProfileQuery({});
+  const [updateMYProfile, { isLoading: updating }] =
+    useUpdateMYProfileMutation();
+
+  return (
+    <>
+      <Container sx={{ mt: 4 }}>
+        <Grid container spacing={4}>
+          <Grid xs={12} md={4}>
+            <Box
+              sx={{
+                height: 300,
+                width: "100%",
+                overflow: "hidden",
+                borderRadius: 1,
+              }}
+            >
+              <Image
+                height={300}
+                width={400}
+                src={data?.profilePhoto}
+                alt="User Photo"
+              />
+            </Box>
+
+            <Button
+              fullWidth
+              endIcon={<ModeEditIcon />}
+              onClick={() => setIsModalOpen(true)}
+            >
+              Edit Profile
+            </Button>
+          </Grid>
+          <Grid xs={12} md={8}>
+            <DoctorInformation data={data} />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default DoctorProfulePage;
