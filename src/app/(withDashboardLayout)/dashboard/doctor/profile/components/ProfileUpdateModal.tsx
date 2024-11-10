e89477e35c56b36f90e0e195bceed353f20695d9
inzamulhaque <mdihalif@gmail.com>
2024-11-10 15:47:36 +0600

    create doctor info update modal

--- src/app/(withDashboardLayout)/dashboard/doctor/profile/components/ProfileUpdateModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/profile/components/ProfileUpdateModal.tsx
@@ -5,9 +5,13 @@ import {
   useUpdateDoctorMutation,
 } from "@/redux/api/doctorApi";
 import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import React, { useState } from "react";
 import { FieldValues } from "react-hook-form";
+import MultipleSelectChip from "./MultipleSelectChip";
+import PHInput from "@/components/Forms/PHInput";
+import PHSelectField from "@/components/Forms/PHSelectField";
+import { Gender } from "@/constant/gender";
 
 type TProps = {
   open: boolean;
@@ -25,12 +29,115 @@ const ProfileUpdateModal = ({ open, setOpen, id }: TProps) => {
 
   const [updateDoctor, { isLoading: updating }] = useUpdateDoctorMutation();
 
-  const handleUpdateProfile = async (values: FieldValues) => {};
+  const handleUpdateProfile = async (values: FieldValues) => {
+    const specialties = selectedSpecialtiesIds.map((specialtiesId: string) => ({
+      specialtiesId,
+      isDeleted: false,
+    }));
+  };
 
   return (
     <>
       <PHFullScreenModal open={open} setOpen={setOpen} title="Update Profile">
         <PHForm submit={handleUpdateProfile} defaultValues={doctorData}>
+          <Grid container spacing={2} sx={{ my: 5 }}>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput name="name" label="Name" sx={{ mb: 2 }} fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="email"
+                type="email"
+                label="Email"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="contactNumber"
+                label="Contract Number"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="address"
+                label="Address"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="registrationNumber"
+                label="Registration Number"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="experience"
+                type="number"
+                label="Experience"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHSelectField
+                items={Gender}
+                name="gender"
+                label="Gender"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="apointmentFee"
+                type="number"
+                label="ApointmentFee"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="qualification"
+                label="Qualification"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="currentWorkingPlace"
+                label="Current Working Place"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <PHInput
+                name="designation"
+                label="Designation"
+                sx={{ mb: 2 }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <MultipleSelectChip
+                allSpecialties={allSpecialties}
+                selectedIds={selectedSpecialtiesIds}
+                setSelectedIds={setSelectedSpecialtiesIds}
+              />
+            </Grid>
+          </Grid>
+
           <Button type="submit" disabled={updating}>
             Save
           </Button>
